476dcc6ff926e8a006bd48f99bbc75ef2f518097
AbhineetMaitrey <sonofam@gmail.com>
2026-02-03 14:59:08 +0530

    Delete event_community_board directory

--- event_community_board/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import profile, register
-from . import views
-
-urlpatterns = [
-    path('profile/', views.profile, name='profile'),
-    path('register/', register),
-]
--- event_community_board/communities/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Community, CommunityMember
-
-admin.site.register(Community)
-admin.site.register(CommunityMember)
